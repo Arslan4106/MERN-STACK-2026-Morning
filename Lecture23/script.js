6971668d8e793770988c84dbf80582edf3802a1c
Muhammad Arslan <116803961+Arslan4106@users.noreply.github.com>
2026-02-24 10:05:45 +0500

    Add files via upload

--- Lecture23/script.js
+++ Lecture23/script.js
@@ -1,2 +1,15 @@
 // DOM Manipulation
 // DOM: Document object Modal
+let randomNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(randomNumber);
+let random = document.querySelector(".secretNumber").textContent;
+console.log(random);
+document.querySelector(".secretNumber").textContent = randomNumber;
+let check = document.querySelector(".check");
+console.log(check);
+
+document.querySelector(".check").addEventListener("click", function () {
+  //   console.log("You have Clicked on Check Button");
+  let guessNumber = document.querySelector(".guess").value;
+  console.log(guessNumber);
+});
