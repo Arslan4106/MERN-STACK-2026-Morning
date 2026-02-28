5196949435cde9e9d289d333f106f6de443625b2
Muhammad Arslan <116803961+Arslan4106@users.noreply.github.com>
2026-02-28 10:26:38 +0500

    Add files via upload

--- Lecture24/script.js
+++ Lecture24/script.js
@@ -1,9 +1,19 @@
 let btnOpenModal = document.querySelectorAll(".btn");
 console.log(btnOpenModal);
 console.log(btnOpenModal.length);
+let modal = document.querySelector(".modal");
+let closeModal = document.querySelector(".closeModal");
+let overlay = document.querySelector(".overlay");
+console.log(modal);
 for (let i = 0; i < btnOpenModal.length; i++) {
   console.log(btnOpenModal[i].textContent);
   btnOpenModal[i].addEventListener("click", function () {
-    console.log("You have Clicked on the Button");
+    // console.log("You have Clicked on the Button");
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
   });
 }
+closeModal.addEventListener("click", function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
